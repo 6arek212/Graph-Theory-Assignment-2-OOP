636651b0566ab429be5acf745da402f1496f7651
tarik <tarik.id.10@hotmail.com>
2021-12-03 13:39:09 +0200

    gui modification

--- src/main/java/assignment2/ui/GraphViewPanel.java
+++ src/main/java/assignment2/ui/GraphViewPanel.java
@@ -35,8 +35,6 @@ public class GraphViewPanel extends JPanel {
     }
 
 
-
-
     /***
      *     getting the node at specific coordinates
      * @param x
@@ -87,31 +85,32 @@ public class GraphViewPanel extends JPanel {
 
 
     private void printGraph(Graphics g) {
-        g.setColor(Color.BLUE);
-        for (int i = 0; i < controller.getEdges().size(); i++) {
-            drawEdge(controller.getEdges().get(i), g);
-        }
-        g.setColor(Color.BLACK);
         for (int i = 0; i < controller.getNodes().size(); i++) {
             drawNode(controller.getNodes().get(i), g);
+        }
+
+        for (int i = 0; i < controller.getEdges().size(); i++) {
+            drawEdge(controller.getEdges().get(i), g);
         }
     }
 
 
     private void drawEdge(EdgeData edgeData, Graphics g) {
+        g.setColor(Color.BLUE);
         NodeData s = controller.getNode(edgeData.getSrc());
         NodeData d = controller.getNode(edgeData.getDest());
         if (s != null && d != null) {
             GeoLocation s0 = world2Frame.worldToframe(s.getLocation());
             GeoLocation d0 = world2Frame.worldToframe(d.getLocation());
-            g.drawLine((int) s0.x(), (int) s0.y(), (int) d0.x(), (int) d0.y());
+            drawArrowLine(g, (int) s0.x(), (int) s0.y(), (int) d0.x(), (int) d0.y(), 10, 5);
         }
     }
 
 
     private void drawNode(NodeData node, Graphics g) {
+        g.setColor(Color.BLACK);
         if (node.getTag() == NodeDataImpl.WHITE) {
-            g.setColor(Color.BLACK);
+            g.setColor(Color.WHITE);
         } else if (node.getTag() == NodeDataImpl.GRAY) {
             g.setColor(Color.GRAY);
         } else {
@@ -124,6 +123,29 @@ public class GraphViewPanel extends JPanel {
         g.fillOval((int) fp.x() - radios / 2, (int) fp.y() - radios / 2, radios, radios);
     }
 
+
+    private void drawArrowLine(Graphics g, int x1, int y1, int x2, int y2, int d, int h) {
+        int dx = x2 - x1, dy = y2 - y1;
+        double D = Math.sqrt(dx * dx + dy * dy);
+        double xm = D - d, xn = xm, ym = h, yn = -h, x;
+        double sin = dy / D, cos = dx / D;
+
+        x = xm * cos - ym * sin + x1;
+        ym = xm * sin + ym * cos + y1;
+        xm = x;
+
+        x = xn * cos - yn * sin + x1;
+        yn = xn * sin + yn * cos + y1;
+        xn = x;
+
+        int[] xpoints = {x2, (int) xm, (int) xn};
+        int[] ypoints = {y2, (int) ym, (int) yn};
+
+        g.drawLine(x1, y1, x2, y2);
+
+        g.setColor(Color.GREEN);
+        g.fillPolygon(xpoints, ypoints, 3);
+    }
 
     public GraphViewModel getController() {
         return controller;
